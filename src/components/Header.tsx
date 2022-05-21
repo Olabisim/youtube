51e0e96ede30526634e27182c0fabcb941a78f64
holabisii <ajoseholabisi@gmail.com>
2022-05-21 18:43:46 -0400

    header component completed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 
 import { StyleSheet as SS, Text as T, View as V } from 'react-native';
-import { Entypo } from '@expo/vector-icons'
+import { Entypo, Ionicons, MaterialIcons } from '@expo/vector-icons'
 import Constant from 'expo-constants'
 
 
 export default function Home() {
+
+        const mycolor = '#212121';
+
         return (
 
                 <V
@@ -12,7 +15,10 @@ export default function Home() {
                         style={{
                                 height: 40,
                                 backgroundColor: 'white',
-                                marginTop: Constant.statusBarHeight
+                                marginTop: Constant.statusBarHeight,
+                                flexDirection: "row",
+                                justifyContent: "space-between",
+                                elevation: 4,
                         }}
                 
                 >
@@ -21,11 +27,13 @@ export default function Home() {
                                         style={{marginLeft: 20}}
                                         name="youtube" size={28} color="red" 
                                 />
-                                <T style={{fontSize: 22, marginLeft: 5}}>Youtube</T>
+                                <T style={{fontSize: 22, marginLeft: 5, fontWeight: 'bold'}}>Youtube</T>
                         </V>
 
-                        <V> 
-
+                        <V style={{flexDirection: "row", justifyContent: "space-around", width: 150, margin: 5}}>  
+                                <Ionicons name="md-videocam" size={32} color={mycolor} />
+                                <Ionicons name="md-search" size={32} color={mycolor} />
+                                <MaterialIcons name="account-circle" size={32} color={mycolor} />
                         </V>
 
                 </V>
